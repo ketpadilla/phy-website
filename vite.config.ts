aa9be05eb72203807dc456bb6807893420db27d4
princejoogie <princejoogie@gmail.com>
2022-10-07 22:03:45 +0800

    fix: remove port from config
    modified:   package.json
    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/phy-website/",
-  server: { port: 3000 },
   plugins: [react()],
 });
