051365ecacf0950a425de551b1f2840dccf5e34b
princejoogie <princejoogie@gmail.com>
2022-10-07 21:36:40 +0800

    fix: base end with /
    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/phy-website",
+  base: "/phy-website/",
   server: { port: 3000 },
   plugins: [react()],
 });
