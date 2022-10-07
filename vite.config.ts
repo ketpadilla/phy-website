0ea28f52454abe0f86da471d2475713f2ca8e53b
princejoogie <princejoogie@gmail.com>
2022-10-07 21:35:26 +0800

    feat: add deploy.sh
    new file:   deploy.sh
    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/phy-website",
   server: { port: 3000 },
   plugins: [react()],
 });
